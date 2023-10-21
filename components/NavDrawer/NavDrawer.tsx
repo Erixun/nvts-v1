7d5a98d3578688842533fc2e25340d67fc89a53b
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-10-21 17:18:28 +0200

    click outside, stoppropagation & map navitems

--- components/NavDrawer/NavDrawer.tsx
+++ components/NavDrawer/NavDrawer.tsx
@@ -1,49 +1,77 @@
 'use client';
 import { Cross2Icon } from '@radix-ui/react-icons';
 import AnimatedLink from '../AnimatedLink/AnimatedLink';
-// import AnimatedLink from 'next/AnimatedLink';
+import { usePathname } from 'next/navigation';
+import { useCallback, useEffect} from 'react';
+
+export const NavItems = [
+  {
+    href: '/',
+    label: 'Home',
+  },
+  {
+    href: '/about',
+    label: 'About',
+  },
+  {
+    href: '/services',
+    label: 'Services',
+  },
+  {
+    href: '/contact',
+    label: 'Contact',
+  },
+];
 
 export function NavDrawer() {
   const closeDrawer = () => {
     document.body.classList.remove('drawer-open');
   };
+
+  const closeDrawerOnClickOutside = useCallback((event: MouseEvent) => {
+    const isDrawerOpen = document.body.classList.contains('drawer-open');
+
+    if (isDrawerOpen) closeDrawer();
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('click', closeDrawerOnClickOutside);
+
+    return () => {
+      document.removeEventListener('click', closeDrawerOnClickOutside);
+    };
+  }, [closeDrawerOnClickOutside]);
+
+  const pathname = usePathname();
+
   return (
     <nav
+      onClick={(e) => e.nativeEvent.stopImmediatePropagation()}
       className="NavDrawer"
       style={{ display: 'grid', paddingBlock: 10, paddingInline: 20 }}
     >
       <div className="NavDrawerHeader">
-        <h2>Sections</h2>
-        {/* <Image src="/logo_wide.svg" width={280} height={80} alt={'nvts logo'} /> */}
+        <h2>Pages</h2>
         <button className="IconButton" aria-label="Close" onClick={closeDrawer}>
           <Cross2Icon />
         </button>
       </div>
-      <ul className="NavDrawerList" style={{ listStyle: 'none' }}>
-        <li className="NavDrawerItem">
-          <AnimatedLink href="/">
-             {/* className="NavDrawerLink"> */}
-            Home
-          </AnimatedLink>
-        </li>
-        <li className="NavDrawerItem">
-          <AnimatedLink href="/about">
-             {/* className="NavDrawerLink"> */}
-            About
-          </AnimatedLink>
-        </li>
-        <li className="NavDrawerItem">
-          <AnimatedLink href="/services">
-             {/* className="NavDrawerLink"> */}
-            Services
-          </AnimatedLink>
-        </li>
-        <li className="NavDrawerItem">
-          <AnimatedLink href="/contact">
-             {/* className="NavDrawerLink"> */}
-            Contact
-          </AnimatedLink>
-        </li>
+      <ul
+        className="NavDrawerList"
+        style={{ listStyle: 'none', paddingLeft: 15 }}
+      >
+        {NavItems.map((item) => (
+          <li
+            className="NavDrawerItem"
+            key={item.href}
+            style={{ position: 'relative' }}
+          >
+            {pathname === item.href && (
+              <span style={{ position: 'absolute', left: -15 }}>&gt;</span>
+            )}
+            <AnimatedLink href={item.href}>{item.label}</AnimatedLink>
+          </li>
+        ))}
       </ul>
     </nav>
   );
